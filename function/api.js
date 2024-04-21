85f7a1c5624d569ea18127c5c942356c148626d1
Elvin <elvintolin@gmail.com>
2024-04-21 11:30:45 +0800

    Initial commit

---
+++ function/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const router = require('./routes/author');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose
+  .connect(process.env.cloudDB || process.env.localDB)
+  .then(()=> console.log('Connected to MongoDB'))
+  .catch((error)=>console.error('Failed to connect to MongoDB'));
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
